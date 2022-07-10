9a5f845abc6109430160b8dc2269e9dee5bc53be
Bruno Masuda <brunokazuaki@gmail.com>
2022-07-10 12:16:40 -0300

    initial commit :tada:

---
+++ app/handlers.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"net/http"
+)
+
+type Customer struct {
+	Name    string `json:"full_name" xml:"full_name"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zip_code" xml:"zip_code'`
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World!!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Bruno", "São Paulo", "02308040"},
+		{"Tatiane", "São Paulo", "02308040"},
+	}
+	// adicionar o content type como json, pois por padrão o content-type da request é text
+
+	// se o content type for igual application/xml, a resposta do request será em xml
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
+}
