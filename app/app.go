61bc5054f02f86456496655fbeddabde2ec52ffb
Bruno Masuda <brunokazuaki@gmail.com>
2022-07-10 14:12:58 -0300

    chore(module): criado módulo app

--- app/app.go
+++ app/app.go
@@ -3,13 +3,21 @@ package app
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Start() {
+	// mux := http.NewServeMux()
+	router := mux.NewRouter()
 	// define routes
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/allcustomers", getAllCustomers)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+	// customer_id é um valor q é passado como var na func getCustomer, esse valor deve uma string de 0 a 9
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 
 	//starting server
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	log.Fatal(http.ListenAndServe("localhost:8000", router))
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ikauzak/go-banking
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0
